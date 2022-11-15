6d55670485a562c5a52313753a080cd65c82dcba
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-15 11:47:46 -0600

    updated files

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-function handleFetch() {
- fetch("https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi")
- .then(res => res.json())
- .then(data => console.log(data))
+fetch('./db.json')
+    .then(resp => resp.json())
+    .then(foodArray => renderFoods(foodArray));
+
+const countryName = document.querySelector("countrys-name")
+const foodName = document.querySelector("food-name")
+const foodImage
+
+function renderFoods(foods){
+  foods.forEach((food)=> {
+  const span = document.createElement("span");
+    span.src = 
+    // how can i add flag images as a span without images in dbjson file.
+    span.addEventListener("click",()=> {
+      countryName.textContent = food.country;
+      foodName.textContent = food.name;
+      foodImage.src = food.image;
+      foodRating.textContent = food.rating;
+    });
+    flagBar.append(span);
+  });
 }
 
-handleFetch();
+
+
+// const images= document.querySelector('#foodImages')
+// const renderingData= (data)=> {
+//     console.log(data)
+//     data.foods.forEach(foods=> {
+//         const imageTag= document.createElement('img')
+//         imageTag.src=foods.image
+//         images.append(imageTag)
+//     })
