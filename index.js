104e6c22c424db33398ef8f3ee62284f170c9583
Kassandra <K1995Moreno@gmail.com>
2022-11-15 09:18:12 -0500

    attempt 2 to merge changes

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-//Attempts 1,, doesnt  work
-// const data= require('./db.json')
-// console.log(data)
-// function getData() {
-//     var json_data = require('./db.json');
-//     for (var i in json_data){
-//     console.log('Name: ' + json_data[i]["name"])
-// }}
-// getData()
 
-//Attempt 2,, doesn't work
-// fetch('./db.json')
-//     .then(resp => resp.json())
-//     .then(data => testingTest(data))
-// function testingTest(data) {
-//     console.log(data)
-// }
+fetch('./db.json')
+    .then(resp => resp.json())
+    .then(data => renderingData(data))
 
+const images= document.querySelector('#foodImages')
 
-//Attempt 3,, many errors but array recieved :::: https://www.youtube.com/watch?v=Z92PqSyUBSI
-// const theData=document.getElementById('theData')
-// import foods from "./db.json" assert {type:'json'}
-//     console.log(foods)
-// const foodsForDisplay= (foods)=> {
-//     foods.forEach(food => {
-//         console.log(food) //Cannot read properties of undefined
-//         const imageTag= document.createElement('img')
-//         imageTag.src=food.imageTag
-//         theData.append(imageTag)
-//})}
-// foodsForDisplay()
+const renderingData= (data)=> {
+    console.log(data)
+    data.forEach(foods=> {
+        const imageTag= document.createElement('img')
+        imageTag.src=foods.image
+        images.append(imageTag)
+    })    
+}
 
 
 
 
-///used links to fgure out how to link db.json without fetch
-// https://stackoverflow.com/questions/40574159/refused-to-execute-script-strict-mime-type-checking-is-enabled
-// https://stackoverflow.com/questions/49481934/fetching-local-json
-// https://flaviocopes.com/fix-cannot-use-import-outside-module/
+
+
+
+
+
+
+
+
+
+
+// const ramensForRating= (ramens) => {
+//     ramens.forEach(ramen => {
+//         const imageTag= document.createElement('img')  
+//         imageTag.src= ramen.image
+//         ramensMenu.append(imageTag)
