d0044fc69e6bb5b6ec7782283900410b13fca362
Kassandra <K1995Moreno@gmail.com>
2022-11-16 12:01:22 -0500

    randomizer created

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 fetch('http://localhost:3000/foods')
     .then(resp => resp.json())
-    .then(foodArray => renderFoods(foodArray));
-
+    .then(foodArray => {
+      renderFoods(foodArray)
+      renderOneFood(foodArray[0])
+    });
 
 const flagBar= document.querySelector("#flag-bar")
 const foodDetails = document.querySelector("#food-detail")
 const randomizer = document.querySelector('#Randomizer')
+const emptyCountry= document.querySelector('#country')
+const emptyName= document.querySelector('#food-name')
+const emptyImage= document.querySelector('#image')
+const emptyRating= document.querySelector('#rating')
 
-
-const renderFoods= ((foodArray) => {
-  ///// adding random() event listener?
+const renderFoods= (foodArray => {
     foodArray.forEach(food => {
         // each return creates an element
         let foodCountry= document.createElement('h2')
@@ -17,46 +21,21 @@ const renderFoods= ((foodArray) => {
         let foodImage= document.createElement('img')
             foodImage.height= 400
             foodImage.width= 500
-        let foodRating= document.createElement('rating')
+        let foodRating= document.createElement('h4')
         // each element gets the .json data attached
         foodCountry.append(food.country)
         foodName.append(food.name)
         foodImage.src= food.image
-        foodRating.append(food.rating)
-        
+        foodRating.append(food.rating)        
         // each element is attached to the food-detail tag
         foodDetails.append(foodCountry)
         foodDetails.append(foodName)
         foodDetails.append(foodImage)
         foodDetails.append(foodRating)
-        console.log(foodCountry, foodName, foodImage, foodRating)
-    
-  
-})})
-renderFoods()
-
-function random(){
-  randomizer.addEventListener("click",(e)=> {
-        foodCountry.textContent = food.country;
-        foodName.textContent = food.name;
-        
-})}
-random()
-
-/* I havn't really looked beyond this point */
-
-flagBar.append(randomizer);
-  let result = document.querySelector('h1');
-  let flags = document.querySelector("#flag-bar");
-  
-// how to get flag img vs text 
-  function getRandomNumber(min,max) {
-    let step1 = max - min + 1; 
-    let step2 = Math.random() * step1;
-    let result = Math.floor(step2) + min;
-    return  result;
-}
-randomizer.addEventListener('click',() => {
-  let index = getRandomNumber(0,flags.length-1);
-  result.textContent = flags[index]
-});
+        })
+      randomizer.addEventListener('click', (e) => {
+        const randomFood = foodArray[Math.floor(Math.random() * foodArray.length)]
+        console.log(randomFood) 
+      })
+})
+          
